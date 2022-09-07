0dda57bfc817d00b6477f3e90b5afd6e33f7adbe
JeronimoDuarte <jeronimoduarte2004@gmail.com>
2022-09-07 20:40:08 -0300

    formularios para insertar datos en la bd completados

--- GruposSociales/urls.py
+++ GruposSociales/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from GruposSociales.views import inicio,amigos,compañeros,familiares,formularioAmigos,formulario_familiares
+from GruposSociales.views import inicio,amigos,compañeros,familiares,formulario_amigos,formulario_familiares,formulario_compañeros
 
 
 
 urlpatterns = [
-    path('',inicio),
+    path('',inicio,name='Inicio'),
     path('amigos/',amigos,name="Amigos"),
     path('familiares/',familiares,name="Familiares"),
     path('compañeros/',compañeros,name="Compañeros"),
-    path('formulario_amigos/',formularioAmigos,name="Formulario de Amigos"),
-    path('formulario',formulario_familiares,name="Formulario de Familiares"),
-    ]
+    path('formulario_amigos/',formulario_amigos,name="Formulario de Amigos"),
+    path('formulario_familiares',formulario_familiares,name="Formulario de Familiares"),
+    path('formulario_compañeros',formulario_compañeros,name="Formulario de Compañeros"),
+]
     
-    #path('formulario',compañerosform,name="Formulario de Amigos"),
+    
