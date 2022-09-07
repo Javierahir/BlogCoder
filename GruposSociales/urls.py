36d99758428155a3c0e83de45f6962bc001c729d
Javierahir <101756484+Javierahir@users.noreply.github.com>
2022-09-07 14:18:07 -0300

    views, urls

--- GruposSociales/urls.py
+++ GruposSociales/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from GruposSociales.views import amigos
+from GruposSociales.views import compañeros
+from GruposSociales.views import familiares
 
 urlpatterns = [
+    path('', amigos),
+    path('', compañeros),
+    path('', familiares),
 ]
