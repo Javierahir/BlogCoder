89b5f7fa43e5db42c12bf79cd60fedc15e7ed1ae
Javier Martinez Riera <javimartinezriera@gmail.com>
2022-09-28 00:51:13 -0300

    arreglo visibilidad views

--- GruposSociales/views.py
+++ GruposSociales/views.py
@@ -5,37 +5,32 @@ from GruposSociales.forms import FormularioAmigos
 
 def inicio(request):
     return render(request, "GruposSociales/inicio.html")
+
 def amigos(request):
    amigos= Amigos.objects.all()
    return render(request, "GruposSociales/amigos.html",{"amigos":amigos})
+
 def compañeros(request):
    return render(request, "GruposSociales/compañeros.html")
+
 def AboutBlog(request):
    return render(request, "GruposSociales/AboutBlog.html",{"AboutBlog":AboutBlog})
+
 def formulario_amigos(request):
-
    if request.method == "POST":
-
       miFormulario = FormularioAmigos(request.POST)
-
-
       if miFormulario.is_valid(): 
-
          informacion = miFormulario.cleaned_data
-
          amigo = Amigos (nombre=informacion['nombre'], apellido=informacion['apellido'],edad=informacion['edad'],email=informacion['email'])
-
          amigo.save()
-
          return render(request, "GruposSociales/inicio.html") 
-
    else:
-
       miFormulario= FormularioAmigos() 
-
    return render(request, "GruposSociales/amigosform.html", {"miFormulario": miFormulario})
+
 def busqueda_amigos(request):
    return render(request,"GruposSociales/busqueda_amigosform.html")
+
 def buscar_amigos(request):
    if request.GET["nombre"]:
       nombre = request.GET["nombre"]
